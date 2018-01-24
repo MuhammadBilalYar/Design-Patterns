326a63db950549d4975078440b103b44542c0426
Muhammad Bilal <mby01@hotmail.com>
2018-01-24 17:41:48 +0500

    Added Singleton design pattern

---
+++ Singleton/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+    {
+    class Program
+        {
+        static void Main (string[] args)
+            {
+            //Created two instance
+            var obj1 = SingletonPolicy.Instance;
+            var obj2 = SingletonPolicy.Instance;
+            Console.WriteLine (obj1 == obj2);
+
+            Console.WriteLine (obj1.GetInsuredName());
+            Console.WriteLine (obj2.GetInsuredName ());
+            }
+        }
+    }
---
+++ Singleton/SingletonPolicy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+    {
+    public class SingletonPolicy
+        {
+        private static readonly SingletonPolicy _instance = new SingletonPolicy ();
+        public static SingletonPolicy Instance
+            {
+            get
+                {
+                return _instance;
+                }
+            }
+        private SingletonPolicy ()
+            {
+
+            }
+        private int Id { get; set; } = 789;
+        private string Insured { get; set; } = "Muhammad Bilal";
+
+        public string GetInsuredName ()
+            {
+            return Insured;
+            }
+        }
+    }
